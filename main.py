60feada2b061a77f6bf97aa470171219170d8f62
Yong Kang Chia <chiayongkang@hotmail.com>
2021-07-23 23:31:52 +0800

    update readme

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import cv2
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
-import streamlit as st
-from streamlit_webrtc import webrtc_streamer
 
-webrtc_streamer(key="example")
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+        return img
+
+
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
